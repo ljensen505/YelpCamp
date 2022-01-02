ccc44df699683b23de69c91b66f68a3331cc432d
lucas <lucas.p.jensen10@gmail.com>
2022-01-01 18:06:41 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+// const mongoose = require('mongoose');
+const app = express();
+const path = require('path');
+
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+})
